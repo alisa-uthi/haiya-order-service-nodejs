178b32178a4e5f47a4e8d643234b97173c73fa75
kimberlinn99 <alisa.uth99@gmail.com>
2021-12-09 18:22:07 +0700

    add route to insert driverId into existing order

--- routes/order_route.js
+++ routes/order_route.js
@@ -1,124 +1,158 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const orderService = require('../services/order_service')
-const deliveryService = require('../services/delivery_service')
-const { OrderStatus } = require('../constants/order_status')
-const { DeliveryStatus } = require('../constants/delivery_status')
+const orderService = require("../services/order_service");
+const deliveryService = require("../services/delivery_service");
+const { OrderStatus } = require("../constants/order_status");
+const { DeliveryStatus } = require("../constants/delivery_status");
 
 // Get order history by order id
-router.get('/:orderId', async (req, res) => {
-    const orderId = req.params.orderId
-    const authorizationToken = req.headers.authorization
-    try {
-        let order = await orderService.getOrderByOrderId(orderId)
+router.get("/:orderId", async (req, res) => {
+  const orderId = req.params.orderId;
+  const authorizationToken = req.headers.authorization;
+  try {
+    let order = await orderService.getOrderByOrderId(orderId);
 
-        if(order) {
-            let orderLines = await orderService.getOrderLinesByOrderId(orderId, authorizationToken)
-            order.orderLines = orderLines
-    
-            return res.status(200).json({ data: order })
-        }
-        
-        return res.status(400).json({ error: "Order not found." })
-    } catch (error) {
-        return res.status(500).json({ error: error.message })
+    if (order) {
+      let orderLines = await orderService.getOrderLinesByOrderId(
+        orderId,
+        authorizationToken
+      );
+      order.orderLines = orderLines;
+
+      return res.status(200).json({ data: order });
     }
-})
+
+    return res.status(400).json({ error: "Order not found." });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
 
 // Get delivery info by order id
-router.get('/:orderId/delivery', async (req, res) => {
-    try {
-        const result = await deliveryService.getDeliveryByOrderId(req.params.orderId)
-        return res.status(200).json({ data: result })
-    } catch (error) {
-        return res.status(500).json({ error: error.message })
-    }
-})
+router.get("/:orderId/delivery", async (req, res) => {
+  try {
+    const result = await deliveryService.getDeliveryByOrderId(
+      req.params.orderId
+    );
+    return res.status(200).json({ data: result });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
 
 // Update order status by order id
-router.patch('/:orderId/status', async (req, res) => {
-    let status = req.body.status.toUpperCase()
+router.patch("/:orderId/status", async (req, res) => {
+  let status = req.body.status.toUpperCase();
 
-    try {
-        if(Object.values(OrderStatus).toString().includes(status)) {
-            const result = await orderService.updateOrderStatus(req.params.orderId, status)
-            if(result == 1) {
-                return res.status(200).json({ data: "Order status has been updated." })
-            }
-        }
-
-        return res.status(400).json({ error: "Invalid order status." })
-    } catch (error) {
-        return res.status(500).json({ error: error.message })
+  try {
+    if (Object.values(OrderStatus).toString().includes(status)) {
+      const result = await orderService.updateOrderStatus(
+        req.params.orderId,
+        status
+      );
+      if (result == 1) {
+        return res.status(200).json({ data: "Order status has been updated." });
+      }
     }
-})
+
+    return res.status(400).json({ error: "Invalid order status." });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+// Update driver of the order by order id
+router.patch("/:orderId/driver", async (req, res) => {
+  const driverId = req.body.driverId;
+
+  try {
+    const result = await orderService.updateOrderDriverPerson(
+      req.params.orderId,
+      driverId
+    );
+    if (result == 1) {
+      return res.status(200).json({ data: "Order's driver has been updated." });
+    }
+    return res.status(500).json({ error: "Invalide order." });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
 
 // Get all order history by user id
-router.get('/user/:userId', async (req, res) => {
-    try {
-        const result = await orderService.getOrderHistoryByUserId(req.params.userId)
-        return res.status(200).json({ data: result })
-    } catch (error) {
-        return res.status(500).json({ error: error.message })
-    }
-})
+router.get("/user/:userId", async (req, res) => {
+  try {
+    const result = await orderService.getOrderHistoryByUserId(
+      req.params.userId
+    );
+    return res.status(200).json({ data: result });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
 
 // Create order by user id
-router.post('/user/:userId', async (req, res) => {
-    const data = req.body
-    const userId = req.params.userId
+router.post("/user/:userId", async (req, res) => {
+  const data = req.body;
+  const userId = req.params.userId;
 
-    try {
-        // Create order
-        const orderId = await orderService.createOrderByUserId(userId, data)
+  try {
+    // Create order
+    const orderId = await orderService.createOrderByUserId(userId, data);
 
-        // Insert order line
-        if(orderId) {
-            for(const index in data.orderLines) {
-                await orderService.createOrderLine(orderId, data.orderLines[index])
-            }
-            return res.status(200).json({ orderId: orderId })
-        }
-        
-        return res.status(400).json({ error: "Unable to create order." })
-    } catch (error) {
-        return res.status(500).json({ error: error.message })
+    // Insert order line
+    if (orderId) {
+      for (const index in data.orderLines) {
+        await orderService.createOrderLine(orderId, data.orderLines[index]);
+      }
+      return res.status(200).json({ orderId: orderId });
     }
-})
+
+    return res.status(400).json({ error: "Unable to create order." });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
 
 // Get all orders by order status
-router.get('/user/:userId/orderStatus/:status', async (req, res) => {
-    try {
-        const { userId, status } = req.params
+router.get("/user/:userId/orderStatus/:status", async (req, res) => {
+  try {
+    const { userId, status } = req.params;
 
-        if(Object.values(OrderStatus).toString().includes(status.toUpperCase())) {
-            let order = await orderService.getOrderByOrderStatus(status, userId) 
-            return res.status(200).json({ data: order })
-        }
-        return res.status(400).json({ error: "Invalid order status." })
-    } catch (error) {
-        return res.status(500).json({ error: error.message })
+    if (Object.values(OrderStatus).toString().includes(status.toUpperCase())) {
+      let order = await orderService.getOrderByOrderStatus(status, userId);
+      return res.status(200).json({ data: order });
     }
-})
+    return res.status(400).json({ error: "Invalid order status." });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
 
 // Create delivery of given order id associated with a driver Id
-router.post('/:orderId/driver/:driverId/delivery', async (req, res) => {
-    const { orderId, driverId } = req.params
-    const deliveryStatus  = req.body.deliveryStatus.toUpperCase()
+router.post("/:orderId/driver/:driverId/delivery", async (req, res) => {
+  const { orderId, driverId } = req.params;
+  const deliveryStatus = req.body.deliveryStatus.toUpperCase();
 
-    try {
-        if(Object.values(DeliveryStatus).toString().includes(deliveryStatus)) {
-            const isInserted = await deliveryService.insertDelivery(orderId, driverId, deliveryStatus)
-            if(isInserted) {
-                return res.status(200).json({ data: 'The order has been ' + deliveryStatus })
-            }
-        }
-
-        return res.status(400).json({ error: "Invalid delivery status." })
-    } catch (error) {
-        return res.status(500).json({ error: error.message })
+  try {
+    if (Object.values(DeliveryStatus).toString().includes(deliveryStatus)) {
+      const isInserted = await deliveryService.insertDelivery(
+        orderId,
+        driverId,
+        deliveryStatus
+      );
+      if (isInserted) {
+        return res
+          .status(200)
+          .json({ data: "The order has been " + deliveryStatus });
+      }
     }
-})
 
-module.exports = router
+    return res.status(400).json({ error: "Invalid delivery status." });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+module.exports = router;
--- services/order_service.js
+++ services/order_service.js
@@ -1,110 +1,140 @@
-const connection = require('../config/database')
-const axios = require('axios')
-const { OrderStatus } = require('../constants/order_status')
+const connection = require("../config/database");
+const axios = require("axios");
+const { OrderStatus } = require("../constants/order_status");
 
 export const getOrderHistoryByUserId = async (userId) => {
-    let query = 'SELECT * FROM `Order` WHERE Ord_Psn_ID = ? AND Ord_OrderStatus = ? '
-    query += 'ORDER BY Ord_PayTimestamp DESC;'
+  let query =
+    "SELECT * FROM `Order` WHERE Ord_Psn_ID = ? AND Ord_OrderStatus = ? ";
+  query += "ORDER BY Ord_PayTimestamp DESC;";
 
-    try {
-        const result = await connection.promise().execute(query, [ userId, OrderStatus.DELIVERED ])
-        return result[0]
-    } catch (error) {
-        throw new Error(`Get Order History By User Id: ${error.message}`)
-    }
-}
+  try {
+    const result = await connection
+      .promise()
+      .execute(query, [userId, OrderStatus.DELIVERED]);
+    return result[0];
+  } catch (error) {
+    throw new Error(`Get Order History By User Id: ${error.message}`);
+  }
+};
 
 export const createOrderByUserId = async (userId, data) => {
-    let query = 'INSERT INTO `Order` '
-    query += '(Ord_DelAddr, Ord_DelPrice, Ord_PayTimestamp, Ord_OrderStatus, Ord_Psn_ID, Ord_Pharmacy) '
-    query += 'VALUES (?, ?, NOW(), ?, ?, ?);'
+  let query = "INSERT INTO `Order` ";
+  query +=
+    "(Ord_DelAddr, Ord_DelPrice, Ord_PayTimestamp, Ord_OrderStatus, Ord_Psn_ID, Ord_Pharmacy) ";
+  query += "VALUES (?, ?, NOW(), ?, ?, ?);";
 
-    try {
-        const result = await connection.promise().execute(
-            query,
-            [
-                data.deliveryAddress, data.deliveryPrice,
-                OrderStatus.CREATED, userId, data.pharmacy
-            ],
-        )
-        return result[0].insertId
-    } catch (error) {
-        throw new Error(`Create Order By User Id: ${error.message}`)
-    }
-}
+  try {
+    const result = await connection
+      .promise()
+      .execute(query, [
+        data.deliveryAddress,
+        data.deliveryPrice,
+        OrderStatus.CREATED,
+        userId,
+        data.pharmacy,
+      ]);
+    return result[0].insertId;
+  } catch (error) {
+    throw new Error(`Create Order By User Id: ${error.message}`);
+  }
+};
 
 export const createOrderLine = async (orderId, item) => {
-    let query = 'INSERT INTO Order_Line (Ord_ID, Prd_ID, Order_Qty, Total_Cost, Comment) '
-    query += 'VALUES (?, ?, ?, ?, ?);'
+  let query =
+    "INSERT INTO Order_Line (Ord_ID, Prd_ID, Order_Qty, Total_Cost, Comment) ";
+  query += "VALUES (?, ?, ?, ?, ?);";
 
-    try {
-        await connection.promise().execute(
-            query,
-            [ orderId, item.productId, item.quantity, item.totalCost, item.comment ],
-        )
-    } catch (error) {
-        throw new Error(`Create Order Lines By Order Id: ${error.message}`)
-    }
-}
+  try {
+    await connection
+      .promise()
+      .execute(query, [
+        orderId,
+        item.productId,
+        item.quantity,
+        item.totalCost,
+        item.comment,
+      ]);
+  } catch (error) {
+    throw new Error(`Create Order Lines By Order Id: ${error.message}`);
+  }
+};
 
 export const getOrderByOrderId = async (orderId) => {
-    let query = 'SELECT * FROM `Order` WHERE ID = ? ;'
+  let query = "SELECT * FROM `Order` WHERE ID = ? ;";
 
-    try {
-        let result = await connection.promise().execute(query, [ orderId ])
-        return result[0][0]
-    } catch (error) {
-        throw new Error(`Get Order By Order Id: ${error.message}`)
-    }
-}
+  try {
+    let result = await connection.promise().execute(query, [orderId]);
+    return result[0][0];
+  } catch (error) {
+    throw new Error(`Get Order By Order Id: ${error.message}`);
+  }
+};
 
 export const getOrderByOrderStatus = async (status, userId) => {
-    let query = 'SELECT * FROM `Order` WHERE Ord_OrderStatus = ? AND Ord_Psn_ID = ? ;'
+  let query =
+    "SELECT * FROM `Order` WHERE Ord_OrderStatus = ? AND Ord_Psn_ID = ? ;";
 
-    try {
-        let result = await connection.promise().execute(query, [ status.toUpperCase(), userId ])
-        return result[0]
-    } catch (error) {
-        throw new Error(`Get Order By Order Id: ${error.message}`)
-    }
-}
+  try {
+    let result = await connection
+      .promise()
+      .execute(query, [status.toUpperCase(), userId]);
+    return result[0];
+  } catch (error) {
+    throw new Error(`Get Order By Order Id: ${error.message}`);
+  }
+};
 
 export const getOrderLinesByOrderId = async (orderId, authorizationToken) => {
-    let query = 'SELECT * FROM `Order_Line` WHERE Ord_ID = ?;'
+  let query = "SELECT * FROM `Order_Line` WHERE Ord_ID = ?;";
 
-    try {
-        const result = await connection.promise().execute(query, [ orderId ])
-        let orderLines = result[0]
+  try {
+    const result = await connection.promise().execute(query, [orderId]);
+    let orderLines = result[0];
 
-        if(orderLines) {
-            // Get product name of each order line
-            for (const index in orderLines) {
-                let product = await axios.get(
-                    `http://inventory-service:8001/product/${orderLines[index].Prd_ID}`,
-                    {
-                        headers: {
-                            Authorization: authorizationToken,
-                        }
-                    }
-                )
-                orderLines[index].Prd_TradeName = product.data.data.Prd_TradeName
-                orderLines[index].Prd_Image = product.data.data.Prd_Image
-            }
-        }
-
-        return orderLines
-    } catch (error) {
-        throw new Error(`Get Order Lines By Order Id: ${error.message}`)
+    if (orderLines) {
+      // Get product name of each order line
+      for (const index in orderLines) {
+        let product = await axios.get(
+          `http://inventory-service:8001/product/${orderLines[index].Prd_ID}`,
+          {
+            headers: {
+              Authorization: authorizationToken,
+            },
+          }
+        );
+        orderLines[index].Prd_TradeName = product.data.data.Prd_TradeName;
+        orderLines[index].Prd_Image = product.data.data.Prd_Image;
+      }
     }
-}
+
+    return orderLines;
+  } catch (error) {
+    throw new Error(`Get Order Lines By Order Id: ${error.message}`);
+  }
+};
 
 export const updateOrderStatus = async (orderId, status) => {
-    let query = 'UPDATE `Order` SET Ord_OrderStatus = ? WHERE ID = ? ;'
+  let query = "UPDATE `Order` SET Ord_OrderStatus = ? WHERE ID = ? ;";
 
-    try {
-        const result = await connection.promise().execute(query, [ status.toUpperCase(), orderId ])
-        return result[0].affectedRows
-    } catch (error) {
-        throw new Error(`Update Order Status By Order Id: ${error.message}`)
-    }
-}
+  try {
+    const result = await connection
+      .promise()
+      .execute(query, [status.toUpperCase(), orderId]);
+    return result[0].affectedRows;
+  } catch (error) {
+    throw new Error(`Update Order Status By Order Id: ${error.message}`);
+  }
+};
+
+export const updateOrderDriverPerson = async (orderId, driverID) => {
+  let query = "UPDATE `Order` SET Ord_Driver_ID = ? WHERE ID = ? ;";
+
+  try {
+    const result = await connection
+      .promise()
+      .execute(query, [driverID, orderId]);
+    return result[0].affectedRows;
+  } catch (error) {
+    throw new Error(`Update Driver of the Order By Order Id: ${error.message}`);
+  }
+};
